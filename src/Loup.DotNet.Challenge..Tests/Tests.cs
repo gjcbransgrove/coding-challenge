39242a0f3fd49021a93c97355b22effeb0c73b32
Guy Bransgrove <guy.bransgrove@live.com>
2022-10-02 20:58:55 +1100

    Add outline for sample response unit tests

--- src/Loup.DotNet.Challenge..Tests/Tests.cs
+++ src/Loup.DotNet.Challenge..Tests/Tests.cs
@@ -1,3 +1,5 @@
+using Loup.DotNet.Challenge.FunctionApp.Models;
+using Loup.DotNet.Challenge.TestFramework;
 using Moq;
 using System.Net;
 using System.Net.Http;
@@ -20,6 +22,30 @@ namespace Loup.DotNet.Challenge.Tests
         {
             _helper = helper;
             _testHost = testHost;
+        }
+
+        [Fact]
+        public async Task When_UserContext_Is_Authenticated_False_Expect_Unauthorized_Template()
+        {
+            
+        }
+
+        [Fact]
+        public async Task When_UserContext_Is_Authenticated_True_Expect_No_Unauthorized_Response()
+        {
+            
+        }
+
+        [Fact]
+        public async Task When_Content_Id_Invalid_Integer_Expect_Bad_Request_Template()
+        {
+            
+        }
+
+        [Fact]
+        public async Task When_Content_Id_In_Repository_Expect_Recipe_Template()
+        {
+            
         }
     }
 }
